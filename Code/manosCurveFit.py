a1622e5b94d5594e7cc0d52d1d47bae1cbafaa7c
Hosea Siu <hoseasiu@mit.edu>
2014-01-09 09:46:56 -0500

    cleaned up run portion of the code- broke out data reading and fitting portions into their own functions

--- Code/manosCurveFit.py
+++ Code/manosCurveFit.py
@@ -1,15 +1,17 @@
 ''' manosCurveFit.py - Hosea Siu 2014
-    Takes light curve data as input and fits it to a function
+    Takes light curve data as input and fits it to a function accoring to Harris et al. 1989
 '''
 
 from numpy import *
 from scipy.optimize import leastsq
 import matplotlib.pyplot as plt, os.path, sys
 
-# class used to store and manipulate MANOS light curve data
+'''class used to store and manipulate MANOS light curve data
+'''
 class lightCurveData:
             
     def getData(self,filename):
+        # to plot, the JD and differential magnitude are always required
 
 # columns are as follows:
 # Image number, Flag, Filter, JD, Airmass, Aperture size, Differential mag, mag error
@@ -41,8 +43,8 @@ class lightCurveData:
         self.magErr = []
         self.getData(filename)        
 
-'''function that allows for range lists of floats
-has some error handling, but still has machine precision issues (shouldn't be an actual problem)
+''' Creates range lists of floats
+    Has some error handling, but still has machine precision issues (shouldn't be an actual problem)
 '''
 def floatRange(start, stop, stepSize=0):    
     if start == stop:
@@ -69,7 +71,15 @@ def floatRange(start, stop, stepSize=0):
         print 'floatRange input error'
         return []
 
-'''generates the light curve model (called by a lambda function later)
+''' Reads in the data file (.txt format) and returns it as a lightCurveData object
+'''
+def readData (filename):
+    basepath = os.path.abspath(os.path.dirname(sys.argv[0]))
+    filepath = os.path.abspath(os.path.join(basepath, "..", 'Data', filename))
+    return lightCurveData(filepath)
+
+
+''' Generates the light curve model (called by a lambda function later)
 '''
 def model(params,t):
     if len(params)<5:
@@ -91,6 +101,34 @@ def model(params,t):
 fitfunc = lambda params, t: model(params,t)             # target function from the model
 errfunc = lambda params, t, mag, err: (fitfunc(params,t)-mag)/err  # distance to the target function
 
+''' Fit the data to a model using scipy.optimize.leastsq.
+    Takes the dataset(a lightCurveData object), the range of orders to check (a two-element list), and an intial guess at the period.
+'''
+def fitData(data, orderRange, initPeriod):
+    # information from light curve for plotting
+    time = data.jd-data.jd[0]
+    mag = data.diffMag
+    err = data.magErr
+
+    bestResult = (-1, -1, array([]))       # storing the best order, error, array
+
+    for m in range(orderRange[0],orderRange[1]):
+            
+        # format the initial parameter guesses
+        params0 = [initPeriod] + [0,0]*m
+
+        params1, success = leastsq(errfunc, params0[:], args=(time,mag,err))
+
+        if success:
+            n = float(len(time))       # number of observations
+            k = float(2*m+1)           # total free parameters
+            var = 1/(n-k)*sum(errfunc(params1, time, mag, err)**2)
+            print 'optimization succeeded for m = ' + str(m) + ', bias-corrected variance = ' + str(var)
+            if bestResult[1] == -1 or bestResult[1] > var:          # check to see if this is a better result
+                bestResult = (m,var,params1)
+        else:
+            print 'optimization failed for m = ' + str(m)
+    return bestResult
 
 ''' Takes the best fit output and the data and plots them
 '''
@@ -100,63 +138,31 @@ def plotAndPrintResults(fit, data):
     err = data.magErr
 
     modelTime = linspace(time.min(), time.max(),100)
-    plt.plot(modelTime, fitfunc(fit[2],modelTime), 'r-')
-    plt.errorbar(time, mag, err, fmt = 'rx')
-    plt.xlabel('Julian Date +' + str(int(min(data.jd)))) # the subtraction simplifies the display of the JD
+    plt.plot(modelTime, fitfunc(fit[2],modelTime), 'r-')    # plot the model
+    plt.errorbar(time, mag, err, fmt = 'rx')                # plot the data
+    plt.xlabel('Julian Date +' + str(int(min(data.jd))))    # the subtraction simplifies display of the JD
     plt.ylabel('Differential Magnitude')
     ax = plt.gca()
-    ax.invert_yaxis()   # flip the y axis
+    ax.invert_yaxis()                                       # flip the y axis
     plt.title(objectName + ' Light Curve, ' + ' T = ' + str(fit[2][0]*24.0) + 'h, m = ' + str(fit[0]))
 
-    print '\n' + 'FITTING RESULTS:'
+    print '\n' + 'FITTING RESULTS:'                         # display results on console
     print 'order = ' + str(fit[0])
     print 'bias-corrected variance = ' + str(fit[1])
     parameters = fit[2].tolist()
     print 'period = ' + str(parameters.pop(0)*24.0) + ' hours'
-    A = parameters[0:len(parameters)/2]
-    B = parameters[len(parameters)/2:len(parameters)+1]
-    print 'A values: ' + str(A)
-    print 'B values: ' + str(B)
+    print 'A values: ' + str(parameters[0:len(parameters)/2])
+    print 'B values: ' + str(parameters[len(parameters)/2:len(parameters)+1])
     plt.show()
-
 
 ###### start of the run routine ######
 
-# the only two lines that should need editing from object to object
+# the only lines that should need editing from object to object
 objectName = 'Spartacus20090130'
 ##objectName = 'da14.120301.568.mag'
 filename = 'Spartacus20090130_MANOS.txt'
-# initial guess at period
-T0 = 0.2
+T0 = 0.2                                # initial guess at period
 
-# read in and format the light curve data from a text file
-basepath = os.path.abspath(os.path.dirname(sys.argv[0]))
-filepath = os.path.abspath(os.path.join(basepath, "..", 'Data', filename))
-lcd = lightCurveData(filepath)
-
-# information from light curve for plotting
-time = lcd.jd-lcd.jd[0]
-mag = lcd.diffMag
-err = lcd.magErr
-
-bestResult = (-1, -1, array([]))       # storing the best order, error, array
-
-for m in range(2,7):        # try 2nd to 6th order fits
-        
-    # format the initial parameter guesses
-    params0 = [T0] + [0,0]*m
-
-    params1, success = leastsq(errfunc, params0[:], args=(time,mag,err))
-
-    if success:
-        n = float(len(time))       # number of observations
-        k = float(2*m+1)           # total free parameters (number of days not counted because we're dong relative photometry only)
-        var = 1/(n-k)*sum(errfunc(params1, time, mag, err)**2)
-        print 'optimization succeeded for m = ' + str(m) + ', bias-corrected variance = ' + str(var)
-        # check to see if this is a better result
-        if bestResult[1] == -1 or bestResult[1] > var:
-            bestResult = (m,var,params1)
-    else:
-        print 'optimization failed for m = ' + str(m)
-
-plotAndPrintResults(bestResult, lcd)    # plot and print the results
+lcd = readData(filename)                # read in data from the text file
+bestFit = fitData(lcd, [2,7], T0)       # fit the data to a model
+plotAndPrintResults(bestFit, lcd)       # plot and print the results
