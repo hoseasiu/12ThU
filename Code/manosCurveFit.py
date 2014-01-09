40967c0da71970a3796d046971ad1e067c6ad738
Hosea Siu <hoseasiu@mit.edu>
2014-01-09 11:50:04 -0500

    added user input options for data formats and user inputs for some plotting options

--- Code/manosCurveFit.py
+++ Code/manosCurveFit.py
@@ -10,38 +10,35 @@ import matplotlib.pyplot as plt, os.path, sys
 '''
 class lightCurveData:
             
-    def getData(self,filename):
-        # to plot, the JD and differential magnitude are always required
-
-# columns are as follows:
-# Image number, Flag, Filter, JD, Airmass, Aperture size, Differential mag, mag error
-
-        data = loadtxt(filename,'a12')          # extract data from text file
+    def getData(self,filename, formatSpec):
+        textFile = loadtxt(filename,'a12')          # extract data from text file
 
         # separate data into appropriately-named arrays
-        self.flag = data[:,1].astype(int)
-        self.filter = data[:,2]
-        self.jd = data[:,3].astype(float)
-        self.airmass = data[:,4].astype(float)
-        self.apSize = data[:,5].astype(float)
-        self.diffMag = data[:,6].astype(float)
-        self.magErr = data[:,7].astype(float)
+        # TODO- currently assumes that everything is a float- generalize later (needed?)
+        for i in range(len(formatSpec)):
+            self.data[formatSpec[i][0]] = textFile[:,formatSpec[i][1]].astype(float)
 
-        #da14
-##        self.jd = data[:,0].astype(float)
-##        self.diffMag = data[:,1].astype(float)
-##        self.magErr = data[:,2].astype(float)
+    # must pass this object a filename, and an n x 2 list of data types and associated columns
+    def __init__(self, objectName, fileName, formatSpec):
+        # check for errors in the format specification
+        if len(formatSpec) <= 0:
+            print 'Error: formatSpec input incorrect'
+            return
+        for i in range(len(formatSpec)):
+            if len(formatSpec[i]) !=2:
+                print 'Error: formatSpec input incorrect'
+                return
         
-    def __init__(self, filename):
-        self.imageNumber = []
-        self.flag = []
-        self.filter = []
-        self.jd = []
-        self.airmass = []
-        self.apSize = []
-        self.diffMag = []
-        self.magErr = []
-        self.getData(filename)        
+        self.name = objectName
+        self.data = {}      # initialize an empty dictionary with the keys in formatSpec
+        for i in range(len(formatSpec)):
+            self.data[formatSpec[i][0]] = {}
+        if 'jd' not in self.data.keys() or 'diffMag' not in self.data.keys() or 'magErr' not in self.data.keys():
+            print 'Error: Insufficient data for light curve'
+            print '       Did you include \'jd\', \'diffMag\', and \'magErr\'?'
+            print '       Right now, I have ' + str(self.data.keys())
+        else:
+            self.getData(fileName, formatSpec)
 
 ''' Creates range lists of floats
     Has some error handling, but still has machine precision issues (shouldn't be an actual problem)
@@ -58,7 +55,7 @@ def floatRange(start, stop, stepSize=0):
         current = start
         while True:
             numbers.append(current)
-            if (start < stop):
+            if (start < stop):                  # allows for forward and backwards listing
                 current = current + stepSize
                 if current > stop:
                     break
@@ -73,10 +70,10 @@ def floatRange(start, stop, stepSize=0):
 
 ''' Reads in the data file (.txt format) and returns it as a lightCurveData object
 '''
-def readData (filename):
+def readData (objectName, fileName, formatSpec):
     basepath = os.path.abspath(os.path.dirname(sys.argv[0]))
-    filepath = os.path.abspath(os.path.join(basepath, "..", 'Data', filename))
-    return lightCurveData(filepath)
+    filepath = os.path.abspath(os.path.join(basepath, "..", 'Data', fileName))
+    return lightCurveData(objectName, filepath, formatSpec)
 
 
 ''' Generates the light curve model (called by a lambda function later)
@@ -98,26 +95,25 @@ def model(params,t):
             H = H + A[L-1]*sin(trigTerm*offset)+B[L-1]*cos(trigTerm*offset)
         return H
 
-fitfunc = lambda params, t: model(params,t)             # target function from the model
-errfunc = lambda params, t, mag, err: (fitfunc(params,t)-mag)/err  # distance to the target function
+'''lambda functions used for the least squares fit
+'''
+fitfunc = lambda params, t: model(params,t)                         # target function from the model
+errfunc = lambda params, t, mag, err: (fitfunc(params,t)-mag)/err   # distance to the target function
 
 ''' Fit the data to a model using scipy.optimize.leastsq.
     Takes the dataset(a lightCurveData object), the range of orders to check (a two-element list), and an intial guess at the period.
 '''
-def fitData(data, orderRange, initPeriod):
+def fitData(lightCurveData, orderRange, initPeriod):
     # information from light curve for plotting
-    time = data.jd-data.jd[0]
-    mag = data.diffMag
-    err = data.magErr
+    time = lightCurveData.data['jd']-lightCurveData.data['jd'][0]
+    mag = lightCurveData.data['diffMag']
+    err = lightCurveData.data['magErr']
 
     bestResult = (-1, -1, array([]))       # storing the best order, error, array
 
     for m in range(orderRange[0],orderRange[1]):
-            
-        # format the initial parameter guesses
-        params0 = [initPeriod] + [0,0]*m
-
-        params1, success = leastsq(errfunc, params0[:], args=(time,mag,err))
+        params0 = [initPeriod] + [0,0]*m    # format the initial parameter guesses
+        params1, success = leastsq(errfunc, params0[:], args=(time,mag,err))    # least squares fit
 
         if success:
             n = float(len(time))       # number of observations
@@ -131,16 +127,24 @@ def fitData(data, orderRange, initPeriod):
     return bestResult
 
 ''' Takes the best fit output and the data and plots them
+    plotting error bars is optional (default is True)
+    plotting the full phase is optional (default is True)
 '''
-def plotAndPrintResults(fit, data):
-    time = data.jd-data.jd[0]
-    mag = data.diffMag
-    err = data.magErr
+def plotAndPrintResults(fit, lightCurveData, plotFullPeriod = True, plotErrors = True):
+    time = lightCurveData.data['jd']-lightCurveData.data['jd'][0]
+    mag = lightCurveData.data['diffMag']
+    err = lightCurveData.data['magErr']
 
-    modelTime = linspace(time.min(), time.max(),100)
+    if plotFullPeriod:
+        modelTime = linspace(time.min(), time.min()+fit[2][0],100)
+    else:
+        modelTime = linspace(time.min(), time.max(),100)
     plt.plot(modelTime, fitfunc(fit[2],modelTime), 'r-')    # plot the model
-    plt.errorbar(time, mag, err, fmt = 'rx')                # plot the data
-    plt.xlabel('Julian Date +' + str(int(min(data.jd))))    # the subtraction simplifies display of the JD
+    if (plotErrors):                                        # plot the data with error bars (default)
+        plt.errorbar(time, mag, err, fmt = 'rx')
+    else:
+        plt.plot(time, mag, 'rx')
+    plt.xlabel('Julian Date +' + str(int(min(lightCurveData.data['jd']))))    # the subtraction simplifies display of the JD
     plt.ylabel('Differential Magnitude')
     ax = plt.gca()
     ax.invert_yaxis()                                       # flip the y axis
@@ -158,11 +162,11 @@ def plotAndPrintResults(fit, data):
 ###### start of the run routine ######
 
 # the only lines that should need editing from object to object
-objectName = 'Spartacus20090130'
-##objectName = 'da14.120301.568.mag'
-filename = 'Spartacus20090130_MANOS.txt'
-T0 = 0.2                                # initial guess at period
+objectName = 'da14' #'Spartacus20090130'
+fileName = 'da14.120301.568.mag.txt' #'Spartacus20090130_MANOS.txt'
+formatSpec = [['jd',0],['diffMag',1],['magErr',2]] #[['jd',3],['diffMag',6],['magErr',7]]
+T0 = 0.25                                # initial guess at period
 
-lcd = readData(filename)                # read in data from the text file
-bestFit = fitData(lcd, [2,7], T0)       # fit the data to a model
-plotAndPrintResults(bestFit, lcd)       # plot and print the results
+lcd = readData(objectName, fileName, formatSpec)        # read in data from the text file
+bestFit = fitData(lcd, [2,7], T0)                       # fit the data to a model
+plotAndPrintResults(bestFit, lcd, False)                       # plot and print the results
