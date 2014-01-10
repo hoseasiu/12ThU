ded8db52f3d79365f6759e091b4e2cbec6fd6c82
Hosea Siu <hoseasiu@mit.edu>
2014-01-09 21:55:54 -0500

    converted to using lmfit minimize package with parameters instead of scipy.optimize- works better, now also has period constraints if needed

--- Code/manosCurveFit.py
+++ Code/manosCurveFit.py
@@ -2,24 +2,29 @@
     Takes light curve data as input and fits it to a function accoring to Harris et al. 1989
 '''
 
-from numpy import *
-from scipy.optimize import leastsq
-import matplotlib.pyplot as plt, os.path, sys
+from lmfit import minimize, Parameters, Parameter, report_fit
+import matplotlib.pyplot as plt, os.path, sys, numpy as np
 
-'''class used to store and manipulate MANOS light curve data
+''' Class used to store and manipulate MANOS light curve data
 '''
 class lightCurveData:
             
-    def getData(self,filename, formatSpec):
-        textFile = loadtxt(filename,'a12')          # extract data from text file
+    def getData(self, filename, formatSpec, magOffset=0):
+        basepath = os.path.abspath(os.path.dirname(sys.argv[0]))
+        filepath = os.path.abspath(os.path.join(basepath, "..", 'Data', fileName))
+
+        textFile = np.loadtxt(filepath,'a12')          # extract data from text file
 
         # separate data into appropriately-named arrays
         # TODO- currently assumes that everything is a float- generalize later (needed?)
         for i in range(len(formatSpec)):
-            self.data[formatSpec[i][0]] = textFile[:,formatSpec[i][1]].astype(float)
+            if formatSpec[i][0] == 'diffMag':
+                self.data[formatSpec[i][0]] = textFile[:,formatSpec[i][1]].astype(float) + magOffset
+            else:
+                self.data[formatSpec[i][0]] = textFile[:,formatSpec[i][1]].astype(float)
 
     # must pass this object a filename, and an n x 2 list of data types and associated columns
-    def __init__(self, objectName, fileName, formatSpec):
+    def __init__(self, objectName, fileName, formatSpec, magOffset = 0.0):
         # check for errors in the format specification
         if len(formatSpec) <= 0:
             print 'Error: formatSpec input incorrect'
@@ -38,7 +43,8 @@ class lightCurveData:
             print '       Did you include \'jd\', \'diffMag\', and \'magErr\'?'
             print '       Right now, I have ' + str(self.data.keys())
         else:
-            self.getData(fileName, formatSpec)
+            self.getData(fileName, formatSpec, magOffset)
+
 
 ''' Creates range lists of floats
     Has some error handling, but still has machine precision issues (shouldn't be an actual problem)
@@ -68,78 +74,88 @@ def floatRange(start, stop, stepSize=0):
         print 'floatRange input error'
         return []
 
-''' Reads in the data file (.txt format) and returns it as a lightCurveData object
+''' Generate the light curve model using Parameters
 '''
-def readData (objectName, fileName, formatSpec):
-    basepath = os.path.abspath(os.path.dirname(sys.argv[0]))
-    filepath = os.path.abspath(os.path.join(basepath, "..", 'Data', fileName))
-    return lightCurveData(objectName, filepath, formatSpec)
-
-
-''' Generates the light curve model (called by a lambda function later)
-'''
-def model(params,t):
-    if len(params)<5:
-        print 'Error: not enough parameters- model must be at least second order'
-        return
-    else:
-        params = params.tolist()
-        P = params.pop(0)
-        A = params[0:len(params)/2]
-        B = params[len(params)/2:len(params)+1]
-
-        H = zeros(len(t))
-        for L in range(1,len(A)+1):
-            trigTerm = 2*pi*L/float(P)
-            offset = t-t[0]
-            H = H + A[L-1]*sin(trigTerm*offset)+B[L-1]*cos(trigTerm*offset)
+def makeModel2(params, t, mag=None, err=None):
+    P = params['P'].value
+    H = np.zeros(len(t))  
+    for L in range(1,len(params.keys())/2+1):
+        trigTerm = 2.0*np.pi*float(L)/float(P)
+        offset = t-t[0]
+        A = params['A'+str(L)].value
+        B = params['B'+str(L)].value
+        H = H + A*np.sin(trigTerm*offset)+B*np.cos(trigTerm*offset)
+    if mag is None and err is None:     # no data given- the model values are returned
         return H
-
-'''lambda functions used for the least squares fit
-'''
-fitfunc = lambda params, t: model(params,t)                         # target function from the model
-errfunc = lambda params, t, mag, err: (fitfunc(params,t)-mag)/err   # distance to the target function
+    elif err is None:                   # magnitude data is given, but no errors- the chi squared is returned
+        return H-mag
+    else:                               # both magnitude and error are given- the reduced chi squared is returned
+        return (H-mag)/err
 
 ''' Fit the data to a model using scipy.optimize.leastsq.
     Takes the dataset(a lightCurveData object), the range of orders to check (a two-element list), and an intial guess at the period.
+    Minimum and maximum values for the period may also be inputs
 '''
-def fitData(lightCurveData, orderRange, initPeriod):
+def fitData(lightCurveData, orderRange, initPeriod, minPeriod = None, maxPeriod = None):
     # information from light curve for plotting
     time = lightCurveData.data['jd']-lightCurveData.data['jd'][0]
     mag = lightCurveData.data['diffMag']
     err = lightCurveData.data['magErr']
 
-    bestResult = (-1, -1, array([]))       # storing the best order, error, array
+    bestResult = None       # storing the best fitting result
+    bestParams = None
+    bestVar = None
+    bestOrder = None
 
-    for m in range(orderRange[0],orderRange[1]):
-        params0 = [initPeriod] + [0,0]*m    # format the initial parameter guesses
-        params1, success = leastsq(errfunc, params0[:], args=(time,mag,err))    # least squares fit
+    for m in range(orderRange[0],orderRange[1]+1):
 
-        if success:
+        params = Parameters()   # create the Parameters object and add P, A, and B values to it
+        params.add('P', value = initPeriod, min = minPeriod, max = maxPeriod)
+        for i in range(m):
+            params.add('A' + str(i+1), value = 0)
+            params.add('B' + str(i+1), value = 0)
+        
+        fitResult = minimize(makeModel2, params, args = (time, mag, err))
+
+        if fitResult.success:
             n = float(len(time))       # number of observations
             k = float(2*m+1)           # total free parameters
-            var = 1/(n-k)*sum(errfunc(params1, time, mag, err)**2)
-            print 'optimization succeeded for m = ' + str(m) + ', bias-corrected variance = ' + str(var)
-            if bestResult[1] == -1 or bestResult[1] > var:          # check to see if this is a better result
-                bestResult = (m,var,params1)
+            var = 1/(n-k)*sum(makeModel2(params, time, mag, err)**2)
+            if bestResult is None:
+                bestResult = fitResult
+                bestParams = params
+                bestVar = var
+                bestOrder = m
+            elif bestVar > var:
+                bestResult = fitResult
+                bestParams = params        
+                bestVar = var
+                bestOrder = m
+            print 'optimization succeeded for m = ' + str(m)
         else:
             print 'optimization failed for m = ' + str(m)
-    return bestResult
+        
+    return (bestResult, bestOrder)
+
 
 ''' Takes the best fit output and the data and plots them
     plotting error bars is optional (default is True)
     plotting the full phase is optional (default is True)
 '''
-def plotAndPrintResults(fit, lightCurveData, plotFullPeriod = True, plotErrors = True):
+def plotAndPrintResults(fit, m, lightCurveData, plotFullPeriod = True, plotErrors = True):
+    report_fit(fit.params)
     time = lightCurveData.data['jd']-lightCurveData.data['jd'][0]
     mag = lightCurveData.data['diffMag']
     err = lightCurveData.data['magErr']
 
+    fittedPeriod = fit.params['P'].value
+
     if plotFullPeriod:
-        modelTime = linspace(time.min(), time.min()+fit[2][0],100)
+        modelTime = np.linspace(time.min(), time.min()+fittedPeriod,100)
     else:
-        modelTime = linspace(time.min(), time.max(),100)
-    plt.plot(modelTime, fitfunc(fit[2],modelTime), 'r-')    # plot the model
+        modelTime = np.linspace(time.min(), time.max(),100)
+
+    plt.plot(modelTime, makeModel2(bestFit.params, modelTime), 'b-')    # plot the model fit
     if (plotErrors):                                        # plot the data with error bars (default)
         plt.errorbar(time, mag, err, fmt = 'rx')
     else:
@@ -148,25 +164,25 @@ def plotAndPrintResults(fit, lightCurveData, plotFullPeriod = True, plotErrors =
     plt.ylabel('Differential Magnitude')
     ax = plt.gca()
     ax.invert_yaxis()                                       # flip the y axis
-    plt.title(objectName + ' Light Curve, ' + ' T = ' + str(fit[2][0]*24.0) + 'h, m = ' + str(fit[0]))
+    plt.title(objectName + ' Light Curve, ' + ' P = ' + str(fit.params['P'].value*24.0) + 'h, m = ' + str(m))
 
-    print '\n' + 'FITTING RESULTS:'                         # display results on console
-    print 'order = ' + str(fit[0])
-    print 'bias-corrected variance = ' + str(fit[1])
-    parameters = fit[2].tolist()
-    print 'period = ' + str(parameters.pop(0)*24.0) + ' hours'
-    print 'A values: ' + str(parameters[0:len(parameters)/2])
-    print 'B values: ' + str(parameters[len(parameters)/2:len(parameters)+1])
     plt.show()
 
 ###### start of the run routine ######
 
 # the only lines that should need editing from object to object
-objectName = 'da14' #'Spartacus20090130'
-fileName = 'da14.120301.568.mag.txt' #'Spartacus20090130_MANOS.txt'
-formatSpec = [['jd',0],['diffMag',1],['magErr',2]] #[['jd',3],['diffMag',6],['magErr',7]]
-T0 = 0.25                                # initial guess at period
+##objectName = 'Spartacus20090130'
+##fileName = 'Spartacus20090130_MANOS.txt'
+##formatSpec = [['jd',3],['diffMag',6],['magErr',7]]
+##magOffset = 0
+##T0 = 0.25                                # initial guess at period
 
-lcd = readData(objectName, fileName, formatSpec)        # read in data from the text file
-bestFit = fitData(lcd, [2,7], T0)                       # fit the data to a model
-plotAndPrintResults(bestFit, lcd, False)                       # plot and print the results
+objectName = 'da14'
+fileName = 'da14.120301.568.mag.txt'
+formatSpec = [['jd',0],['diffMag',1],['magErr',2]]
+magOffset = -18.5
+T0 = 0.25
+
+lcd = lightCurveData(objectName, fileName, formatSpec, magOffset)       # read in data from the text file
+(bestFit, m) = fitData(lcd, [2,6], T0)                                  # fit the data to a model (can also add min and max periods in JD)
+plotAndPrintResults(bestFit, m, lcd)                                    # plot and print the results
